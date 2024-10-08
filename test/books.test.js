051be1269e84c89700582ab4e018367555debd63
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-08 10:01:04 +0200

    testone ok

--- test/books.test.js
+++ test/books.test.js
@@ -2,14 +2,19 @@ import request from "supertest";
 import { app , server } from "../app.js";
 import conection_db from "../database/conectionDb.js";
 
-
 describe ("crud books", ()=>{
     test("should return a response with status 200 and type json", async ()=>{
         const response = await request (app).get("/books");
         expect(response.statusCode).toBe(200);
         expect(response.header["content-type"]).toContain("application/json")
     });
-    afterAll(()=>{
+ 
+    // test("should return a response with status 201 and type json", async ()=>{
+    //     const response = await request (app).post("/books");
+    //     expect(response.statusCode).toBe(200);
+    //     expect(response.header["content-type"]).toContain("application/json")
+
+    afterAll(() => {
         server.close();
         conection_db.close();
     });
